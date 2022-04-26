b41c049065ca4c64fc5831c0fc73c5cfc7500244
Mutlaq Aldhbuiub <matulkn@gmail.com>
2022-04-26 21:06:37 +0300

    Fix the image height and width that makes it looks fatty xD

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
 
-var imageWidth = 500;
-var imageHeight = 700;
+var imageWidth = 1080;
+var imageHeight = 1920;
 
 var imageObj = new Image(imageWidth,imageHeight);
 
@@ -23,7 +23,7 @@ function DownloadCanvasAsImage(){
     });
 }
 
-imageObj.src = "assets/images/cover.jpg"; 
+imageObj.src = "assets/images/oldcover.jpg"; 
 
 var downloadCardButton = document.getElementById('downloadCard');
 downloadCardButton.addEventListener('click', function(e){
@@ -35,14 +35,14 @@ downloadCardButton.addEventListener('click', function(e){
 
     // custom font
     context.textAlign = 'center';
-    context.font = "25pt IBMPlexSansArabic";
+    context.font = "50pt IBMPlexSansArabic";
 
     // text color
     context.fillStyle = '#262967';
 
     // center and make text
     textWidth = (imageWidth - imageWidth / 2)
-    textHeight = (imageHeight - 80)
+    textHeight = (imageHeight - 190)
     
     context.fillText(text, textWidth, textHeight);
 
