ebfe4f458e05e02824d141371f4f1d1a449554cf
Mutlaq Aldhbuiub <matulkn@gmail.com>
2022-04-27 23:00:12 +0300

    Update the size of font, change the text position

--- assets/js/index.js
+++ assets/js/index.js
@@ -42,7 +42,7 @@ downloadCardButton.addEventListener('click', function(e){
 
     // center and make text
     textWidth = (imageWidth - imageWidth / 2)
-    textHeight = (imageHeight - 280)
+    textHeight = (imageHeight - 400)
     
     context.fillText(text, textWidth, textHeight);
 
